32492ecdf343fcd548fdffe36c6be73c0a9ebcf3
Charlie Edwards <charles.edwards1811@gmail.com>
2021-08-26 16:51:27 +1000

    ranking added

--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 
 const Rank = ({name, entries}) => {
+  const [emoji, setEmoji] = useState('')
+
+  const generateEmoji = useCallback(async (entries) => { 
+     const data = await fetch(`https://mm3e61swpl.execute-api.us-east-1.amazonaws.com/prod/rank?rank=${entries}`)
+     const resp = await data.json()
+     setEmoji(resp.input)
+  }, [])
+  useEffect(() => { 
+    generateEmoji(entries)
+  }, [generateEmoji, entries])
+
   return (
      <div>
        <div className='white f3'> 
@@ -8,6 +19,9 @@ const Rank = ({name, entries}) => {
        </div>
        <div className='white f1'> 
          {`#${entries}`}
+       </div>
+       <div className='white f3'> 
+         {`Rank Badge: ${emoji}`}
        </div>
      </div>
   );
